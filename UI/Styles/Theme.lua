04e679c2d0ca832975735d17410a641e7070b258
SinnyTime <108436063+SinnyTime@users.noreply.github.com>
2025-05-07 20:27:33 -0400

    Create Theme.lua

---
+++ UI/Styles/Theme.lua
@@ -0,0 +1,8 @@
+return {
+	BackgroundColor = Color3.fromRGB(25, 25, 25),
+	SectionColor = Color3.fromRGB(35, 35, 35),
+	TextColor = Color3.fromRGB(235, 235, 235),
+	AccentColor = Color3.fromRGB(0, 170, 255),
+	Font = Enum.Font.GothamSemibold,
+	CornerRadius = UDim.new(0, 10)
+}
